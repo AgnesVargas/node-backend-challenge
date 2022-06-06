4fbe53e0cc0cedd5e7b36a28736ee8cadd2c6a68
Agnes <99425726+AgnesVargas@users.noreply.github.com>
2022-06-06 16:12:52 -0300

    up

--- src/presentation/controllers/UserController.js
+++ src/presentation/controllers/UserController.js
@@ -1,8 +1,16 @@
 class UserController {
-
     handle(req, res, next) {
-        res.send("Hello World!");
+        res.send({
+            nome_completo: req.body.nome_completo,
+            email: req.body.email,
+            confemail: req.body.confemail,
+            cpf: req.body.cpf,
+            celular: req.body.celular,
+            nasc: req.body.nasc,
+        });
+        req.send({
+            
+        })
     }
 }
-
 module.exports = new UserController();
